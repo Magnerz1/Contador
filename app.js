c4e9177c4dfba95627383b2d30046a4d5ae91149
Magnerz1 <doritoopscorpion@gmail.com>
2024-05-30 00:36:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const load = () =>{
+
+};
+
+document.load = load;
+
+/** Guarda valores en el local storage */
+const set = (key, value) => {
+    localStorage.setItem(key. value)
+};
+const get =() => {
+    if (key) {
+        throw new Error ("")
+    }
+};
+// adadadad
+// adad
+// asdasdadas
+// adadaaLL
